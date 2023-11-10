b62178d1bc52f3a164e0373e85de856a7ba22085
pysyve <94847523+pysyve0730@users.noreply.github.com>
2023-11-10 15:55:00 +0800

    Create mipi_rffe.h

---
+++ mipi_rffe.h
@@ -0,0 +1,13 @@
+#ifndef MIPI_RFFE_H_
+#define MIPI_RFFE_H_
+
+struct REG_0{
+    int trigger = 0x00;
+    int band_select = 0x00;
+    int pa_enable = 0x01;
+    int pa_mode = 0x00;
+    int lb_switch = 0x01;
+};
+
+
+#endif
