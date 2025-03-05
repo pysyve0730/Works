e18c12e9b3f26bdd1b470f392afc83f8f69d0cb3
pysyve <94847523+pysyve0730@users.noreply.github.com>
2025-03-05 14:56:52 +0800

    Create filter_port8883_start_end_packet_time_write_seperate.py

---
+++ filter_port8883_start_end_packet_time_write_seperate.py
@@ -0,0 +1,102 @@
+import re
+from openpyxl import Workbook
+
+#packet_number connot repeat, so delete repeat data
+
+
+f = open("X5A_0401_A010_new.txt", "r",encoding='UTF-8')
+
+
+lines = f.readlines()
+tmp = []
+hand_shake_type = []
+info = []
+start_pkg_num = []
+start_pkg_time = []
+end_pkg_num = []
+end_pkg_time = []
+
+
+source_pkg_num = 6
+source_pkg_time = 1
+dist_pkg_num = 8
+dist_pkg_time = 1
+
+for line in lines:
+    #print(line) #Print all information
+    tmp = re.split(r'\[', line)
+
+    if(len(tmp)==2):
+        info = tmp[0].split()
+        hand_shake_type = tmp[1].split()
+        print(info)
+        if len(info)==9:
+            
+            if "8883" in info and "SYN]" in hand_shake_type[0]:
+                start_pkg_num.append(info[source_pkg_num])
+                start_pkg_time.append(info[source_pkg_time])
+
+            elif "8883" in info and ("FIN," in hand_shake_type[0] or "RST," in hand_shake_type[0]):
+                end_pkg_num.append(info[dist_pkg_num])
+                end_pkg_time.append(info[dist_pkg_time])
+
+start_dict = dict(zip(start_pkg_num, start_pkg_time))
+print("Start has "+str(len(start_dict))+" rows:"+str(start_dict))
+
+print("=======================================")
+
+end_dict = dict(zip(end_pkg_num, end_pkg_time))
+print("End has "+str(len(end_dict))+" rows:"+str(end_dict))
+
+
+
+def save_to_excel(data, filename):
+    wb = Workbook()
+    ws = wb.active
+    for packet_num, packet_time in data.items():
+        ws.append([packet_num, packet_time])
+    wb.save(filename)
+
+save_to_excel(start_dict, "output_0401_start.xlsx")
+save_to_excel(end_dict, "output_0401_end.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+        
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
